eb2efd5bf7615a74d71f1cda3be071ddba39068c
Yurii Panchenko <yuriy.panchenko@md.p2h.com>
2022-10-07 21:32:19 +0200

    hw3

---
+++ wh3/js/main.js
@@ -0,0 +1,8 @@
+let num1 = +prompt("Please enter first number", '');
+let num2 = +prompt("Please enter second number", '');
+
+alert(`Result додавання =  ${num1 + num2}`);
+alert(`Result віднімання =  ${num1 - num2}`);
+alert(`Result множення =  ${num1 * num2}`);
+alert(`Result ділення =  ${num1 / num2}`);
+
