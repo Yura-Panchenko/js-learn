c877db887b917adf50898c233204ab3533fbabeb
Yurii Panchenko <yuriy.panchenko@md.p2h.com>
2022-10-13 22:00:22 +0200

    home work 10

---
+++ wh10/js/main.js
@@ -0,0 +1,23 @@
+// Створити масив, довжину та елементи якого задає користувач.
+let arr = [];
+let el;
+do{
+    el = prompt('Enter nuber','');
+    if(el !== null) {
+        arr.push(Number(el));
+        console.log(arr);
+    }
+}while(el !== null);
+console.log(arr);
+
+// Відсортувати масив за зростанням.
+arr.sort((a,b) => {
+    return a - b;
+})
+console.log(arr);
+
+// Видалити елементи з масиву з 2 по 4 (включно!).
+arr.splice(1,3);
+console.log(arr);
+
+// У міру змін виводити вміст масиву на сторінку.
