6a58c4502377b7978c44bb685f4630df896231a2
Yurii Panchenko <yuriy.panchenko@md.p2h.com>
2022-10-20 22:24:34 +0200

    fix wh14

--- wh14/js/main.js
+++ wh14/js/main.js
@@ -4,18 +4,14 @@
 // Наприклад:
 
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-const key = generateKey(10, characters);
+const key = generateKey(5, characters);
 
 function generateKey(length, characters) {
-    let chArr = characters.split('');
-    // let nevArr = [];
     let str = '';
     for(let i = 0; i < length ; i++) {
-        let index = Math.round(Math.random() * chArr.length);
-        // nevArr.push(chArr[index]);
+        let index = Math.round(Math.random() * characters.split('').length);
         str += characters[index];
     }
-    // return nevArr.join('');
     return str;
 }
 console.log(key);
