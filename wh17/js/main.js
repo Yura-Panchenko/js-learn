6a3973f97001f8e4e4ccb1a14abc6d48752f33ec
Yurii Panchenko <yuriy.panchenko@md.p2h.com>
2022-10-21 21:32:48 +0200

    wh17

---
+++ wh17/js/main.js
@@ -0,0 +1,12 @@
+// Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д. 
+// Все це із замиканнями, наприклад: sum(3) = 3 sum(5) = 8 sum(20) = 28
+function sumFunc(n) {
+    return function(number) {
+        return n += number;
+    }
+}
+const sum = sumFunc(1);
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
