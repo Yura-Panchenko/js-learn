698e3e7b2dd620027a74e612e13fe7fb40684549
Yurii Panchenko <yuriy.panchenko@md.p2h.com>
2022-12-10 21:10:49 +0100

    hw30

--- wh30/index.js
+++ wh30/index.js
@@ -11,11 +11,96 @@
 
 const register = document.getElementById('register');
 const save = document.getElementById('save');
+const userName = document.getElementById('name');
+const data = document.getElementById('data');
+const city = document.getElementById('city');
+const address = document.getElementById('address');
+const sex = document.querySelectorAll('input[name="sex"]');
+const lang = document.querySelectorAll('input[name="lang"]');
+const infoWrap = document.getElementById('info');
+const info = document.querySelector('.table tbody tr');
+const obj = {};
 
-console.log(register.elements);
+function createTable(event) {
+    event.preventDefault();
 
-function createTable() {
-    // const inputs = document.
+    if(userName.value != '') {
+        obj.userName = userName.value;
+        userName.classList.remove('border-danger');
+        userName.classList.add('border-success');
+    }else{
+        userName.classList.add('border-danger');
+        userName.classList.remove('border-success');
+        return false;
+    }
+
+    if(data.value != '') {
+        obj.data = data.value;
+        data.classList.remove('border-danger');
+        data.classList.add('border-success');
+    }else{
+        data.classList.add('border-danger');
+        data.classList.remove('border-success');
+        return false;
+    }
+
+    sex.forEach(el => {
+        if(el.checked){
+            obj.sex = el.value;
+        }
+    })
+
+    if(city.selectedIndex) {
+        obj.city = city.value;
+        city.classList.remove('border-danger');
+        city.classList.add('border-success');
+    }else{
+        city.classList.add('border-danger');
+        city.classList.remove('border-success');
+        return false;
+    }
+
+    if(address.value) {
+        obj.address = address.value;
+        address.classList.remove('border-danger');
+        address.classList.add('border-success');
+    }else{
+        address.classList.add('border-danger');
+        address.classList.remove('border-success');
+        return false;
+    }
+
+    let langStr = '<ol>';
+    lang.forEach(el => {
+        if(el.checked) {
+            langStr += `<li>${el.value}</li>`;
+        }
+    })
+    langStr += '</ol>';
+    obj.languiges = langStr;
+
+    register.classList.add('d-none')
+    infoWrap.classList.remove('d-none');
+
+    const tdUser = document.createElement('td');
+    tdUser.textContent = obj.userName
+    const tdData = document.createElement('td');
+    tdData.textContent = obj.data
+    const tdSex = document.createElement('td');
+    tdSex.textContent = obj.sex
+    const tdCity = document.createElement('td');
+    tdCity.textContent = obj.city
+    const tdAddress = document.createElement('td');
+    tdAddress.textContent = obj.address
+    const tdLanguiges = document.createElement('td');
+    tdLanguiges.innerHTML = obj.languiges;
+
+    info.append(tdUser);
+    info.append(tdData);
+    info.append(tdSex);
+    info.append(tdCity);
+    info.append(tdAddress);
+    info.append(tdLanguiges);
 }
 
 save.addEventListener('click', createTable);
